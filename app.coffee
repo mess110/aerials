7a446c766640fb52d2b4f5e70b0d89ad3b1e2813
Cristian Mircea Messel <mess110@gmail.com>
2016-12-14 03:00:23 +0200

    add mesh

--- app.coffee
+++ app.coffee
@@ -3,7 +3,7 @@ config = Config.get()
 # config.toggleDebug()
 config.toggleStats()
 config.fillWindow()
-config.transparentBackground = true
+# config.transparentBackground = true
 
 engine = new Engine3D()
 
@@ -12,11 +12,11 @@ class GameScene extends BaseScene
     @common()
 
     postavarul = SaveObjectManager.get().items['poiana.converted']
-
+    minAlt = undefined
     for point in postavarul
       minAlt = point.alt if point.alt < minAlt || !minAlt?
 
-    dotMaterial = new (THREE.PointsMaterial)(size: 2, color: 'green')
+    dotMaterial = new (THREE.PointsMaterial)(size: 1, color: 'white')
     dotGeometry = new (THREE.Geometry)
 
     for point in postavarul
@@ -31,12 +31,44 @@ class GameScene extends BaseScene
     dot.position.x = - box.max.x / 2
     dot.position.z = - box.max.z / 2
 
+    i = 0
+    last_lat = postavarul[0].lon
+    items_per_row = 0
+    for point in postavarul
+      if last_lat != point.lon
+        items_per_row = i
+        break
+      i += 1
+    console.log postavarul.size()
+    console.log items_per_row
+    console.log postavarul.size() / items_per_row
+
+    geometry = new (THREE.PlaneBufferGeometry)(box.max.x, box.max.z, 384, 396)
+    vertices = geometry.attributes.position.array
+    i = -1
+    j = 0
+    while i < vertices.length
+      vertices[i] = postavarul[j].alt - minAlt
+      i += 3
+      j += 1
+    geometry.computeVertexNormals()
+    geometry.applyMatrix (new (THREE.Matrix4)).makeRotationX(-Math.PI / 2)
+    material = new (THREE.MeshPhongMaterial)(color: 0x009900)
+    texture = TextureManager.get().items['cloud']
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+    texture.repeat.set( 10, 10 )
+    material = new (THREE.MeshPhongMaterial)(map: texture)
+    ground = new (THREE.Mesh)(geometry, material)
+    ground.position.y = -1000
+    @scene.add ground
+
   common: ->
     engine.camera.position.set 0, 2500, 5000
     Helper.orbitControls(engine)
 
-    @scene.add Helper.ambientLight()
-    @scene.add Helper.ambientLight()
+    # @scene.add Helper.ambientLight()
+    # @scene.add Helper.ambientLight()
+    @scene.add Helper.light()
     @scene.add Helper.ambientLight()
 
   tick: (tpf) ->
@@ -49,6 +81,7 @@ gameScene = new GameScene()
 
 loadingScene = new LoadingScene([
   'poiana.converted.save.json'
+  'assets/cloud.png'
 ], () ->
   engine.initScene(gameScene)
 )
--- app.js
+++ app.js
@@ -9,8 +9,6 @@ config.toggleStats();
 
 config.fillWindow();
 
-config.transparentBackground = true;
-
 engine = new Engine3D();
 
 GameScene = (function(superClass) {
@@ -21,22 +19,23 @@ GameScene = (function(superClass) {
   }
 
   GameScene.prototype.init = function() {
-    var box, dot, dotGeometry, dotMaterial, i, j, len, len1, minAlt, point, postavarul;
+    var box, dot, dotGeometry, dotMaterial, geometry, ground, i, items_per_row, j, k, l, last_lat, len, len1, len2, m, material, minAlt, point, postavarul, texture, vertices;
     this.common();
     postavarul = SaveObjectManager.get().items['poiana.converted'];
-    for (i = 0, len = postavarul.length; i < len; i++) {
-      point = postavarul[i];
-      if (point.alt < minAlt || (typeof minAlt === "undefined" || minAlt === null)) {
+    minAlt = void 0;
+    for (k = 0, len = postavarul.length; k < len; k++) {
+      point = postavarul[k];
+      if (point.alt < minAlt || (minAlt == null)) {
         minAlt = point.alt;
       }
     }
     dotMaterial = new THREE.PointsMaterial({
-      size: 2,
-      color: 'green'
+      size: 1,
+      color: 'white'
     });
     dotGeometry = new THREE.Geometry;
-    for (j = 0, len1 = postavarul.length; j < len1; j++) {
-      point = postavarul[j];
+    for (l = 0, len1 = postavarul.length; l < len1; l++) {
+      point = postavarul[l];
       point.alt -= minAlt;
       dotGeometry.vertices.push(new THREE.Vector3(point.lat, point.alt, point.lon));
     }
@@ -44,14 +43,50 @@ GameScene = (function(superClass) {
     this.scene.add(dot);
     box = new THREE.Box3().setFromObject(dot);
     dot.position.x = -box.max.x / 2;
-    return dot.position.z = -box.max.z / 2;
+    dot.position.z = -box.max.z / 2;
+    i = 0;
+    last_lat = postavarul[0].lon;
+    items_per_row = 0;
+    for (m = 0, len2 = postavarul.length; m < len2; m++) {
+      point = postavarul[m];
+      if (last_lat !== point.lon) {
+        items_per_row = i;
+        break;
+      }
+      i += 1;
+    }
+    console.log(postavarul.size());
+    console.log(items_per_row);
+    console.log(postavarul.size() / items_per_row);
+    geometry = new THREE.PlaneBufferGeometry(box.max.x, box.max.z, 384, 396);
+    vertices = geometry.attributes.position.array;
+    i = -1;
+    j = 0;
+    while (i < vertices.length) {
+      vertices[i] = postavarul[j].alt - minAlt;
+      i += 3;
+      j += 1;
+    }
+    geometry.computeVertexNormals();
+    geometry.applyMatrix((new THREE.Matrix4).makeRotationX(-Math.PI / 2));
+    material = new THREE.MeshPhongMaterial({
+      color: 0x009900
+    });
+    texture = TextureManager.get().items['cloud'];
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(10, 10);
+    material = new THREE.MeshPhongMaterial({
+      map: texture
+    });
+    ground = new THREE.Mesh(geometry, material);
+    ground.position.y = -1000;
+    return this.scene.add(ground);
   };
 
   GameScene.prototype.common = function() {
     engine.camera.position.set(0, 2500, 5000);
     Helper.orbitControls(engine);
-    this.scene.add(Helper.ambientLight());
-    this.scene.add(Helper.ambientLight());
+    this.scene.add(Helper.light());
     return this.scene.add(Helper.ambientLight());
   };
 
@@ -67,7 +102,7 @@ GameScene = (function(superClass) {
 
 gameScene = new GameScene();
 
-loadingScene = new LoadingScene(['poiana.converted.save.json'], function() {
+loadingScene = new LoadingScene(['poiana.converted.save.json', 'assets/cloud.png'], function() {
   return engine.initScene(gameScene);
 });
 
